e89370e046c682d38941ae45de5d6a8ceabac919
danilocalixto77 <danilocalixto@gmail.com>
2024-09-22 08:33:15 -0400

    Aula 26: 202409220833

---
+++ Scripts/Aula26ForB.m
@@ -0,0 +1,8 @@
+clear
+clc
+#Iniciar no 1, salta de 1(quando nao informa o incremento o default é 1, e irá até 100
+soma=0;
+for k=1:1:100
+  soma =
+  #fprintf('O quadrado de %d é %d\n',k, x) #\n quebra linha
+endfor
